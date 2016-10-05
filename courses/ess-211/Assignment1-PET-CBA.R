b6101fc073022ce59dbb68c32a46b968b530f455
Christopher Anderson <cbanderson08@gmail.com>
2016-10-04 23:02:55 -0700

    prepared baseline functions for assignment 1

---
+++ courses/ess-211/Assignment1-PET-CBA.R
@@ -0,0 +1,142 @@
+# ESS-211 assignment 1, due 10-12-16
+#  cba 10/2016
+
+###
+# below are the functions provided in PET.helper.functions.r
+###
+
+# saturated vapor pressure for a given temperature
+e0 <- function(temp){
+    0.6108*exp(17.27*temp/(temp+237.3))
+}
+
+# this is how FAO suggests one do it for daily esat b/c of nonlinearity of e0 function 
+esat <- function(tmax,tmin){ 
+    (e0(tmax)+e0(tmin))/2  
+}
+
+# slope of saturation curve
+s <- function(temp){
+    4098*(0.6108*exp(17.27*temp/(temp+237.3)))/(temp+237.3)^2    
+}
+
+# extraterrestrial net radiation (MJ / m^2 / day)
+Ra <- function(doy,lat){   #lat in decimal degrees       #p79 FAO
+  psi=(pi/180)*lat
+  delta=0.409*sin((2*pi*doy/365)-1.39)
+  ws=acos(-tan(psi)*tan(delta))
+  dr=1+0.033*cos(2*pi*doy/365)
+  Ra=(24*60/pi)*0.082*dr*(ws*sin(psi)*sin(delta)+cos(psi)*cos(delta)*sin(ws))
+  Ra
+}
+
+# psychrometric constant [kPa C-1]
+psychro <- function(elev){             #elev in m
+  press=101.3*((293-0.0065*elev)/293)^5.26
+  0.665e-3*press
+}
+
+# daylength (returns # hours in the day with sunlight. only for areas between 65N-6)
+daylength <-function(lat, doy){
+  psi=(pi/180)*lat
+  delta=0.409*sin((2*pi*doy/365)-1.39)
+  ws=acos(-tan(psi)*tan(delta))
+  d=24*ws/pi
+  d
+}
+ 
+# net surface radiation
+Rn <- function(lat, elev, doy, dew, tmax, tmin, Rs){
+  ea=e0(dew)
+  Ra=Ra(doy,lat)
+  Rso=(0.75+(2e-5)*elev)*Ra          #p85
+  Rnl=4.903e-9*(((tmax+273)^4+(tmin+273)^4)/2)*(0.34-0.14*sqrt(ea))*(1.35*(Rs/Rso)-0.35)  #p86
+  Rn=(1-0.23)*Rs-Rnl             #(Rns=(1-0.23)Rs) ; Rn=Rns-Rnl p87
+  Rn
+}
+
+###
+# below are various functions by cba
+###
+
+tMean <- function(tMin, tMax){
+    tMean <- (tMin + tMax)/2
+}
+
+###
+# below are the 6 methods for calculating PET
+###
+
+# method 1: priestly taylor
+#  base algorithm - PET = a*((s*(Rn-G))/(lambda*(s+gamma)))
+#  a = 1.26 (constant)
+#  s = slope of vapor pressure curve
+#  gamma = psychrometric constant
+#  G = ground heat flux (assumed to equal 0)
+#  lambda = 2.501 - 0.002361 * Tmean
+
+priestlyTaylor <- function(doy, tMax, tMin, RH, tDew, Rs, elev, lat, lon){
+    # define constant 'a'
+    a <- 1.26
+ 
+    # calculate the mean temperature for the day
+    meanTemp <- tMean(tMin, tMax)
+ 
+    # calculate slope of vapor pressure curve using temp above
+    vpSlope <- s(meanTemp)
+ 
+    # calculate the psychometric constant
+    gamma <- psychro(elev)
+    
+    # set the ground heat flux to zero
+    groundHeatFlux <- 0
+    
+    # calculate net radiation
+    netRadiation <- Rn(lat, elev, doy, tDew, tMax, tMin, Rs)
+ 
+    # set lambda
+    lambda <- 2.501 - (0.002361 * meanTemp)
+    
+    # run the full calculation
+    PET <- a * ((vSlope * (netRadiation - groundHeatFlux)) / 
+      (lambda * (vpSlope + gamma)))
+    PET
+}
+
+# method 2: modified priestly taylor
+
+
+###
+# below are variables and commands to run each function using the following info
+#  Report PET according to each method for:
+#  date : Aug 21, 2013
+#  Tmax : 27.7 C
+#  Tmin : 13.3 C
+#  RH   : 67%
+#  Tdew : 13.9C
+#  Rs   : 22.5 MJ/m^2
+###
+
+# set day of year using julian calendar
+doy <- 233
+
+# set max temp
+tMax <- 27.7
+
+# set min temp
+tMin <- 13.3
+
+# set dew temp
+tDew <- 13.9
+
+# set relative humidity
+RH <- 0.67
+
+# set the input solar radiation
+Rs <- 22.5
+
+# set elevation and lat/lon from this website
+#  http://www.latlong.net/place/gilroy-ca-usa-6030.html
+elev <- 61
+lat <- 37.005783
+lon <- -121.568275
