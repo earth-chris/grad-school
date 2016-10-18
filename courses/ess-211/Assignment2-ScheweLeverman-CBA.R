1580b03540a6b104277fbe81c82e0a2e2a74f01a
Christopher Anderson <cbanderson08@gmail.com>
2016-10-17 18:35:28 -0700

    updated assignment 2 with function

--- courses/ess-211/Assignment2-ScheweLeverman-CBA.R
+++ courses/ess-211/Assignment2-ScheweLeverman-CBA.R
@@ -5,9 +5,6 @@
 #############################
 # below are the functions used for this assignment
 
-# set up a function to calculate memory effect using time from tau
-ismMemory <- function(day, tau, pSeason)
-
 # set up the ISM rainfall function as translated from matlab
 ismRainfall <- function(lSeason, pStrong, pWeak,
                         tau, prMax, pInit){
@@ -18,17 +15,46 @@ ismRainfall <- function(lSeason, pStrong, pWeak,
   # prMax    : maximum probability of either state
   # pInit    : initial probability of strong state.
   #            must be <= prMax
+  
+  ###
   # we're going to try vectorizing this whole code and use which()
   #  statements so no for/while/ifs are necessary
   
-  # create a vector that will store the daily precip. for the year
+  # create a vector to store the season's precipitatoin values
   pSeason <- rep(NA, lSeason)
   
-  # create a vector to store the random numbers
+  # create a vector to store random probabilities
   pr <- runif(lSeason)
   
-  # return cumulative precip
-  return(pSum)
+  # create a vector to store modeled wet/dry probabilities
+  p <- rep(NA, lSeason)
+  
+  ###
+  # determine probabilities based on memory effect
+  
+  # set [tau] number of days to initial probability of strong state
+  p[1:tau] <- pInit
+  
+  # loop through all values after tau to calculate memory-based probability
+  for (i in (tau+1):lSeason){
+    p[i] <- ((sum(p[(i-tau):i-1]) / tau) - pWeak) / (pStrong - pWeak)
+  }
+  
+  ###
+  # limit the high and low probabilities based on maximum probability threshold
+  p[p > prMax] <- prMax
+  p[p < (1-prMax)] <- 1-prMax
+  
+  ###
+  # assign precipitation values
+  pSeason[pr < p] <- pStrong
+  pSeason[pr >= p] <- pWeak
+  
+  # calculate the mean precipitation
+  pMean <- sum(pSeason) / lSeason
+  
+  # return mean precip for the wet season
+  return(pMean)
 }
 #############################
 # below is the script to generate the model results
