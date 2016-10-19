3f56cca3ed83f303400749fa105988c0b17f3027
Christopher Anderson <cbanderson08@gmail.com>
2016-10-18 18:23:27 -0700

    finalized assignment 2 code and plot

--- courses/ess-211/Assignment2-ScheweLeverman-CBA.R
+++ courses/ess-211/Assignment2-ScheweLeverman-CBA.R
@@ -2,11 +2,11 @@
 #  cba 10/2016
 #
 # Question 1:
-#  First year default precip : 5.86666666666667 mm/day"
-#  100th year default precip : 6.13333333333333 mm/day"
+#  First year default precip : 3.8 mm/day"
+#  100th year default precip : 6.06666666666667 mm/day"
 
 #############################
-# below are the functions used for this assignment
+# below is the function used for this assignment
 
 # set up the ISM rainfall function as translated from matlab
 ismRainfall <- function(inputVector, lSeason=135){
@@ -16,7 +16,7 @@ ismRainfall <- function(inputVector, lSeason=135){
   # [3[ tau      : memory length in time steps (days)
   # [4] prMax    : maximum probability of either state.
   # [5] pInit    : initial probability of strong state.
-  #            must be <= prMax
+  #                must be <= prMax
   # lSeason  : length of the wet season (days). default = 135
   
   # split the input vector for legibility
@@ -27,38 +27,33 @@ ismRainfall <- function(inputVector, lSeason=135){
   prInit >- inputVector[5]
   
   ###
-  # create the vectors to store outputs
-  
   # create a vector to store the season's precipitation values
   P <- rep(0, lSeason)
-  
-  # create a vector to store random probabilities
-  pr <- runif(lSeason)
-  
-  # create a vector to store modeled wet/dry probabilities
-  p <- rep(0, lSeason)
   
   ###
   # loop through days in a season to determine rainfall
   #  sadly, I could not find a way to vectorize this without breaking something
   for (n in 1:lSeason) {
     
+    # set a random number
+    pr <- runif(1)
+    
     # determine probability based on memory effect
     if (n > tau){
-		  p[n] <- (sum(P[(n-tau):n-1]) / tau - pWeak) / (pStrong - pWeak)
+      p <- (sum(P[(n-tau):(n-1)]) / tau - pWeak) / (pStrong - pWeak)
 	  } else {
-	  	p[n] <- prInit
+	  	p <- prInit
 	  }
 	
 	  # limit high and low probabilities based on max probability
-	  if (p[n] > prMax) {
-	  	p[n] <- prMax
-	  } else if (p[n] < (1-prMax)) {
-	  	p[n] <- 1-prMax
+	  if (p > prMax) {
+	  	p <- prMax
+	  } else if (p < (1-prMax)) {
+	  	p <- 1-prMax
 	  }
 
   	# assign precip values based on probability deviation from random
-  	if (pr[n] < p[n]) { 
+  	if (pr < p) { 
   		P[n] <- pStrong
   	} else {
   		P[n] <- pWeak
@@ -90,16 +85,20 @@ testVector <- c(pStrong, pWeak, tau, prMax, prInit)
 pastYears <- 6030
 futureYears <- 250
 
-# we'll apply this as a matrix, so create a 5 x nYears matrix to apply to
-testMatrix <- t(matrix(rep(testVector, pastYears), nrow = length(testVector)))
+# create a vector to store the outputs for the first model runs
+defaultVector <- rep(NA, pastYears)
 
-# apply the function to the testMatrix
+# set the seed
 set.seed(5489)
-test <- apply(testMatrix, 1, ismRainfall)
+
+# loop through each year and calculate Pmean
+for (i in 1:pastYears){
+  defaultVector[i] <- ismRainfall(testVector)
+}
 
 # report the first and 100th year's precipitation
-print(paste("First year default precip :", test[1], "mm/day"))
-print(paste("100th year default precip :", test[100], "mm/day"))
+print(paste("First year default precip :", defaultVector[1], "mm/day"))
+print(paste("100th year default precip :", defaultVector[100], "mm/day"))
 
 #############################
 # PROBLEM 2
@@ -107,26 +106,29 @@ print(paste("100th year default precip :", test[100], "mm/day"))
 
 # 1. current climate
 s1vec <- c(9.0, 0.0, 17, 0.8, 0.75)
-s1mat <- t(matrix(rep(s1vec, pastYears), nrow = length(s1vec)))
+s1 <- rep(0, pastYears)
 
 # 2. 2150-2200 climate
 s2vec <- c(10.9, 1.9, 17, 0.82, 0.2)
-s2mat <- t(matrix(rep(s2vec, futureYears), nrow = length(s2vec)))
+s2 <- rep(0, futureYears)
 
 # 3. high atmospheric saturation
 s3vec <- c(10.9, 1.9, 17, 0.8, 0.75)
-s3mat <- t(matrix(rep(s3vec, pastYears), nrow = length(s3vec)))
+s3 <- rep(0, pastYears)
 
 # 4. changing sea level pressure
-s4vec <- c(9.0, 0.0, 0.8, 0.2)
-s4mat <- t(matrix(rep(s4vec, pastYears), nrow = length(s4vec)))
+s4vec <- c(9.0, 0.0, 17, 0.8, 0.2)
+s4 <- rep(0, pastYears)
 
-# run each of the four scenarios using apply
-set.seed(5489)
-s1 <- apply(s1mat, 1, ismRainfall)
-s2 <- apply(s2mat, 1, ismRainfall)
-s3 <- apply(s3mat, 1, ismRainfall)
-s4 <- apply(s4mat, 1, ismRainfall)
+# run each of the four scenarios 
+for (i in 1:pastYears){
+  s1[i] <- ismRainfall(s1vec)
+  s3[i] <- ismRainfall(s3vec)
+  s4[i] <- ismRainfall(s4vec)
+}
+for (i in 1:futureYears){
+  s2[i] <- ismRainfall(s2vec)
+}
 
 #############################
 # create the four-panel figures
@@ -154,7 +156,7 @@ s4color <- 'green'
 
 # set the titles for each plot
 s1title <- "Current Climate"
-s2title <- "2150-2020 Climate"
+s2title <- "2150-2200 Climate"
 s3title <- "High Atmospheric Sat."
 s4title <- "Delta Sea Level Pressure"
 
@@ -163,7 +165,7 @@ s1plot <- hist(s1, breaks = breaks, xlab = xlab, ylab = ylabPast, main = s1title
 abline(v = quantile(s1)[2:4], lty = 2)
 abline(v = mean(s1), lwd = 3)
 
-s3plot <- hist(s3, breaks = breaks, xlab = xlab, ylab = ylabFuture, main = s3title, col = s3color)
+s3plot <- hist(s3, breaks = breaks, xlab = xlab, ylab = ylabPast, main = s3title, col = s3color)
 abline(v = quantile(s3)[2:4], lty = 2)
 abline(v = mean(s3), lwd = 3)
 
@@ -171,12 +173,9 @@ s2plot <- hist(s2, breaks = breaks, xlab = xlab, ylab = ylabFuture, main = s2tit
 abline(v = quantile(s2)[2:4], lty = 2)
 abline(v = mean(s2), lwd = 3)
 
-s4plot <- hist(s4, breaks = breaks, xlab = xlab, ylab = ylabFuture, main = s4title, col = s4color)
+s4plot <- hist(s4, breaks = breaks, xlab = xlab, ylab = ylabPast, main = s4title, col = s4color)
 abline(v = quantile(s4)[2:4], lty = 2)
 abline(v = mean(s4), lwd = 3)
-
-# add a legend
-legend('topright', lty = c(1,2), lwd = c(3,1), legend = c('Mean', 'Quartiles'))
 
 # and add a title
 title('Schewe-Levermann Indian Summer Monsoon Rainfall', outer = TRUE)
