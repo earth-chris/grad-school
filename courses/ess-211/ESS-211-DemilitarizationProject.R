8dbcce6b243737ffe3a10b8668699304bed61a92
Christopher Anderson <cbanderson08@gmail.com>
2016-11-26 18:10:12 -0800

    added new plot

--- courses/ess-211/ESS-211-DemilitarizationProject.R
+++ courses/ess-211/ESS-211-DemilitarizationProject.R
@@ -13,16 +13,41 @@ setwd("~/src/aei-grad-school/courses/ess-211/")
 source("ESS-211-Functions.R")
 
 # load the raw data to a data frame
-enlistmint <- read.csv('demilitarization_project/Enlistment.csv', header = TRUE)
+enlistment <- read.csv('demilitarization_project/Enlistment.csv', header = TRUE)
 demographics <- read.csv('demilitarization_project/Demographics.csv', row.names = 1, header = TRUE)
 yearly <- read.csv('demilitarization_project/YearlyData.csv', header = TRUE)
 
-# exploratory plots
+# append no-data to the data frames for years we will model
+finalYear = 2100
+startYear = max(yearly$Year) + 1
+
+# loop through each year
+for (year in seq(startYear, finalYear)){
+  
+  # create the NA vectors to append
+  yearVec <- c(year, rep(NA, ncol(yearly) - 1))
+  enlistVec <- c(year, rep(NA, ncol(enlistment) - 1))
+  
+  # append the new rows
+  yearly <- rbind(yearly, yearVec)
+  enlistment <- rbind(enlistment, enlistVec)
+}
+
+#############################
+# create some exploratory plots
+
+# number of unique conflicts per year
 cols <- rep("", nrow(yearly))
 cols[yearly$PresidentialParty == "Republican"] = "Red"
 cols[yearly$PresidentialParty == "Democrat"] = "Blue"
 ylab <- "Number of Unique Conflicts"
 xlab <- "Year"
 title <- "Unique Military Conflicts Per Year"
+barplot(yearly$nConflicts, col = add.alpha(cols, 0.6), names.arg = yearly$Year, ylab = ylab, xlab = xlab, main = title)
 
-barplot(yearly$nConflicts, col = cols, names.arg = yearly$Year, ylab = ylab, xlab = xlab, main = title)
+# average conflicts by party
+cols <- c("Red", "Blue")
+xlab <- c("Republican", "Democrat")
+title <- "Unique Military Conflicts by Presidential Party"
+conflictList <- list(yearly$nConflicts[yearly$PresidentialParty == "Republican"], yearly$nConflicts[yearly$PresidentialParty == "Democrat"])
+boxplot(conflictList, col = add.alpha(cols, 0.6), names = xlab, main = title, ylab = ylab)
