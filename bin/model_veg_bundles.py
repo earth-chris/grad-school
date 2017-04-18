af126fedaae4aabbf49b9fb5e5540ae0f68506f7
Christopher Anderson <cbanderson08@gmail.com>
2017-04-18 10:06:18 -0700

    updated loop and parameter settings

--- bin/model_veg_bundles.py
+++ bin/model_veg_bundles.py
@@ -4,7 +4,7 @@
 # for a suite of vegetation types for use in unmixing models and in
 # canopy reflectance inversions
 #
-# c. 2016 Christopher Anderson
+# c. 2016-2017 Christopher Anderson
 #####
 
 import numpy as np
@@ -27,117 +27,111 @@ s = SixS()
 output_base = aei.params.environ['AEI_GS'] + '/scratch/spectral_libraries/sr_test_fullrange_veg_bundles'
 
 # set number of random veg, bundles to simulate
-n_bundles = 500
+n_bundles = 300
 
-# set the number of output bands (default prosail is 2151)
+# set the number of output bands (default prosail is 2101)
 nb = 2101
 
 #####
 # set up the leaf and canopy modeling parameters
 #####
 
-# structural coefficient (arbitrary units)
-#  range 1.3 - 2.5 from Rivera et al. 2013 http://dx.doi.org/10.3390/rs5073280
-#N = aei.fn.randomFloats(n_bundles, 1.3, 2.5)
+# set up the lists for each leaf/canopy parameter
 N = []
-for i in range(n_bundles):
-    N.append(random.uniform(1.3,2.5))
-
-# total chlorophyll content (ug/cm^2)
-#  range ~ 5 - 75 from Rivera et al. 2013
-#chloro = aei.fn.randomFloats(n_bundles, 10, 60)
 chloro = []
-for i in range(n_bundles):
-    chloro.append(random.gauss(35, 30))
-    while chloro[-1] < 5 or chloro[-1] > 75:
-        chloro[-1] = random.gauss(35, 30)
-
-# total carotenoid content (ug/cm^2)
-caroten = aei.fn.randomFloats(n_bundles, 8, 8)
-
-# brown pigment content (arbitrary units)
-brown = aei.fn.randomFloats(n_bundles, 0, 0)
-
-# equivalent water thickness (cm)
-#  range 0.002 - 0.05 from Rivera et al. 2013
-#EWT = aei.fn.randomFloats(n_bundles, 0.002, 0.05)
+caroten = []
+brown = []
 EWT = []
-for i in range(n_bundles):
-    EWT.append(random.uniform(0.002, 0.05))
-
-# leaf mass per area (g/cm^2)
-#  global range 0.0022 - 0.0365 (median 0.01)
-#  from Asner et al. 2011 http://dx.doi.org/10.1016/j.rse.2011.08.020
-#LMA = aei.fn.randomFloats(n_bundles, 0.005, 0.035)
 LMA = []
-for i in range(n_bundles):
-    LMA.append(random.gauss(0.012, 0.005))
-    while LMA[-1] < 0.0022 or LMA[-1] > 0.0365:
-        LMA[-1] = random.gauss(0.012, 0.005)
-
-# soil reflectance metric (wet soil = 0, dry soil = 1)
 soil_reflectance = []
-for i in range(n_bundles):
-    soil_reflectance.append(random.uniform(0,1))
-
-# leaf area index (unitless, cm^2 leaf area/cm^2 ground area)
-#  range 0.01 - 18.0 (5.5 mean) globally
-#  range 0.2 - 8.7 (3.6 mean) for crops
-#  range 0.6 - 2.8 (1.3 mean) for desert plants
-#  range 0.5 - 6.2 (2.6 mean) for boreal broadleaf forest
-#  range 0.5 - 8.5 (4.6 mean) for boreal needle forest
-#  range 0.8 - 11.6 (5.1 mean) for temperate broadleaf forest
-#  range 0.01 - 15.0 (5.5 mean) for temperate needle forest
-#  range 0.6 - 8.9 (4.8 mean) for tropical broadleaf forest
-#  range 0.3 - 5.0 (1.7 mean) for grasslands
-#  range 1.6 - 18.0 (8.7 mean) for plantations
-#  range 0.4 - 4.5 (2.1 mean) for shrublands
-#  range 0.2 - 5.3 (1.9 mean) for tundra
-#  range 2.5 - 8.4 (6.3 mean) for wetlands
-#  from Asner, Scurlock and Hicke 2003 http://dx.doi.org/10.1046/j.1466-822X.2003.00026.x
-#LAI = aei.fn.randomFloats(n_bundles, 0.6, 12.0)
 LAI = []
-for i in range(n_bundles):
-    LAI.append(random.gauss(3,2))
-    while LAI[-1] < 0.1 or LAI[-1] > 18:
-        LAI[-1] = random.gauss(3,2)
-
-# hot spot parameter (derived from brdf model)
-#  range 0.05-0.5 from Rivera et al. 2013
-#hot_spot = aei.fn.randomFloats(n_bundles, 0.05, 0.5)
 hot_spot = []
-for i in range(n_bundles):
-    hot_spot.append(random.uniform(0.05, 0.5))
-
-# leaf distribution function parameter.
-#  range LAD_inc -0.4 -  0.4, LAD_bim -0.1 - 0.2 for trees
-#  range LAD_inc -0.1 -  0.3, LAD_bim  0.3 - 0.5 for lianas
-#  range LAD_inc -0.8 - -0.2, LAD_bim -0.1 - 0.3 for Palms
-#  from Asner et al. 2011
-#LAD_inclination = aei.fn.randomFloats(n_bundles, 0., 0.8) - 0.4
-#LAD_bimodality = aei.fn.randomFloats(n_bundles, 0., 0.3) - 0.1
 LAD_inclination = []
 LAD_bimodality = []
-for i in range(n_bundles):
-    LAD_inclination.append(random.uniform(-0.4, 0.4))
-    LAD_bimodality.append(random.uniform(-0.1, 0.2))
-
-# old leaf inclination parameters based on fixed canopy architecture. options include:
-# Planophile, Erectophile, Plagiophile, Extremophile, Spherical, Uniform
-# LIDF = [pyprosail.Planophile, pyprosail.Uniform]
-# LIDF_ind = np.random.random_integers(0,len(LIDF)-1,n_iterations)
-
-# viewing and solar angle parameters
-#  solar zenith ranges cludged from http://gis.stackexchange.com/questions/191692/maximum-solar-zenith-angle-for-landsat-8-images
-#  I couldn't find good data on the range of possible solar or viewing azimuth.
-#  I decided to set view parameters to 0 to assume nice, clean nadir viewing, and let the sun vary.
 s_az = []
 s_za = []
 v_az = []
 v_za = []
+
+# randomly sample from pool of parameters
 for i in range(n_bundles):
-    s_az.append(random.uniform(20, 70))
-    s_za.append(random.uniform(0,360))
+    # structural coefficient (arbitrary units)
+    #  range 1.3 - 2.5 from Rivera et al. 2013 http://dx.doi.org/10.3390/rs5073280
+    N.append(random.uniform(1.3,2.5))
+
+    # total chlorophyll content (ug/cm^2)
+    #  range ~ 5 - 75 from Rivera et al. 2013
+    chloro.append(random.gauss(35, 30))
+    while chloro[-1] < 5 or chloro[-1] > 75:
+        chloro[-1] = random.gauss(35, 30)
+
+    # total carotenoid content (ug/cm^2)
+    #  converted range from Asner et al. 2011 http://dx.doi.org/10.1016/j.rse.2011.08.020
+    #  to ug/cm^2 from mg/g 
+    caroten.append(random.gauss(7.5, 6.5))
+    while caroten[-1] < 1.5 or caroten[-1] > 15:
+        caroten[-1] = random.gauss(7.5, 6.5)
+    
+    # brown pigment content (arbitrary units)
+    #  fix to zero - no range found to use
+    brown.append(0)
+
+    # equivalent water thickness (cm)
+    #  range 0.002 - 0.05 from Rivera et al. 2013
+    EWT.append(random.uniform(0.002, 0.05))
+
+    # leaf mass per area (g/cm^2)
+    #  global range 0.0022 - 0.0365 (median 0.01)
+    #  from Asner et al. 2011 http://dx.doi.org/10.1016/j.rse.2011.08.020
+    LMA.append(random.gauss(0.012, 0.005))
+    while LMA[-1] < 0.0022 or LMA[-1] > 0.0365:
+        LMA[-1] = random.gauss(0.012, 0.005)
+
+    # soil reflectance metric (wet soil = 0, dry soil = 1)
+    soil_reflectance.append(random.uniform(0,1))
+
+    # leaf area index (unitless, cm^2 leaf area/cm^2 ground area)
+    #  range 0.01 - 18.0 (5.5 mean) globally
+    #  range 0.2 - 8.7 (3.6 mean) for crops
+    #  range 0.6 - 2.8 (1.3 mean) for desert plants
+    #  range 0.5 - 6.2 (2.6 mean) for boreal broadleaf forest
+    #  range 0.5 - 8.5 (4.6 mean) for boreal needle forest
+    #  range 0.8 - 11.6 (5.1 mean) for temperate broadleaf forest
+    #  range 0.01 - 15.0 (5.5 mean) for temperate needle forest
+    #  range 0.6 - 8.9 (4.8 mean) for tropical broadleaf forest
+    #  range 0.3 - 5.0 (1.7 mean) for grasslands
+    #  range 1.6 - 18.0 (8.7 mean) for plantations
+    #  range 0.4 - 4.5 (2.1 mean) for shrublands
+    #  range 0.2 - 5.3 (1.9 mean) for tundra
+    #  range 2.5 - 8.4 (6.3 mean) for wetlands
+    #  from Asner, Scurlock and Hicke 2003 http://dx.doi.org/10.1046/j.1466-822X.2003.00026.x
+    LAI.append(random.gauss(3,2))
+    while LAI[-1] < 0.1 or LAI[-1] > 18:
+        LAI[-1] = random.gauss(3,2)
+
+    # hot spot parameter (derived from brdf model)
+    #  range 0.05-0.5 from Rivera et al. 2013
+    hot_spot.append(random.uniform(0.05, 0.5))
+
+    # leaf distribution function parameter.
+    #  range LAD_inc -0.4 -  0.4, LAD_bim -0.1 - 0.2 for trees
+    #  range LAD_inc -0.1 -  0.3, LAD_bim  0.3 - 0.5 for lianas
+    #  range LAD_inc -0.8 - -0.2, LAD_bim -0.1 - 0.3 for Palms
+    #  from Asner et al. 2011
+    LAD_inclination.append(random.uniform(-0.4, 0.4))
+    LAD_bimodality.append(random.uniform(-0.1, 0.2))
+
+    # old leaf inclination parameters based on fixed canopy architecture. options include:
+    # Planophile, Erectophile, Plagiophile, Extremophile, Spherical, Uniform
+    # LIDF = [pyprosail.Planophile, pyprosail.Uniform]
+    # LIDF_ind = np.random.random_integers(0,len(LIDF)-1,n_iterations)
+
+    # viewing and solar angle parameters
+    #  solar zenith ranges cludged from http://gis.stackexchange.com/questions/191692/maximum-solar-zenith-angle-for-landsat-8-images
+    #  I couldn't find good data on the range of possible solar or viewing azimuth.
+    #  I decided to set view parameters to 0 to assume nice, clean nadir viewing, and let the sun vary.
+    s_az.append(random.uniform(0, 360))
+    s_za.append(random.uniform(20, 20))
     v_az.append(0)
     v_za.append(0)
 
