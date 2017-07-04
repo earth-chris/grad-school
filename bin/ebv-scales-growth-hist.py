1eea96a19165e1a06964a68221128abad2af9d08
Christopher Anderson <cbanderson08@gmail.com>
2017-07-03 17:11:35 -0700

    dont remember updates

--- bin/ebv-scales-growth-hist.py
+++ bin/ebv-scales-growth-hist.py
@@ -6,6 +6,7 @@
 import aei
 import gdal
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap as lsc
 from matplotlib.colors import ColorConverter as cc
@@ -24,6 +25,8 @@ ofile = base + 'CR-histograms.tif'
 names = ['TreeCover', 'NIRv', 'Temperature', 'Insolation', 'CloudCover']
 titles = ['Tree Cover (%)', 'NIRv (unitless)', 'Temperature (C)',
     'Solar Insolation (kWh / m2)', 'Annual Cloud Cover (%)']
+cor_titles = ['Tree Cover\n(%)', 'NIRv\n(unitless)', 'Temperature\n(C)',
+    'Solar Insolation\n(kWh / m2)', 'Annual Cloud Cover (%)']
 
 # create color maps for each plot
 tree_cmap = lsc.from_list('tree_cmap', [(0, cc.to_rgba('#cc78a7')), (1, cc.to_rgba('#06de37'))])
@@ -100,14 +103,27 @@ arr = data[0:4, gd[0], gd[1]]
 
 # create heat maps using hexbin to plot correlations between each variable
 ctable = 'plasma'
-stanford_cmap = temp_cmap = lsc.from_list('stanford_cmap', [(0, cc.to_rgba("#4D4F53")), (1, cc.to_rgba("#8C1515"))])
+#ctable = 'copper'
+stanford_cmap = temp_cmap = lsc.from_list('stanford_cmap', [(0, cc.to_rgba("#000000")), (1, cc.to_rgba("#8C1515"))])
+
 plt.figure(2)
 
 # set up the plots to use
-plots = [[0,0], [0,1], [0,2], [0,3], [1,1], [1,2], [1,3], [2,2], [2,3], [3,3]]
+#plots = [[0,0], [0,1], [0,2], [0,3], [1,1], [1,2], [1,3], [2,2], [2,3], [3,3]]
+plots = [[0,1], [0,2], [0,3], [1,2], [1,3], [2,3]]
 for plot in plots:
     #plt.subplot(441)
-    plt.subplot(4, 4, 1 + plot[0] + (plot[1] * 4))
+    #plt.subplot(4, 4, 1 + plot[0] + (plot[1] * 4))
+    plt.subplot(3, 3, 1 + plot[0] + ((plot[1]-1) * 3))
     plt.hexbin(arr[plot[0]], arr[plot[1]], gridsize = 20, cmap = ctable,
         alpha = 0.95, linewidths = 0, bins = 'log')
+    
+    # label the correlation
+    plt.title("pearson's r: {:0.2f}".format(cor[plot[0], plot[1]]))
+        
+    # label the axes
+    if plot[0] == 0:
+        plt.ylabel(cor_titles[plot[1]])
+    if plot[1] == 3:
+        plt.xlabel(cor_titles[plot[0]])
     
